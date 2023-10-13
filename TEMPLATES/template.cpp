d746ebe126e265fcde2f904b16362fb170453b70
Josue Romero <josueromram3@gmail.com>
2023-10-13 18:59:47 -0500

    Templates

---
+++ TEMPLATES/template.cpp
@@ -0,0 +1,35 @@
+/**
+   ░░█ █▀█ █▀ █░█ █▀▀ █▀█ █▀█ █▀▄▀█
+   █▄█ █▄█ ▄█ █▄█ ██▄ █▀▄ █▄█ █░▀░█
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define ll  long long
+#define pb  push_back
+#define br  '\n'
+
+void solve() {
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   int tt = 1;
+   cin >> tt;
+   for (int nc = 1; nc <= tt; nc++) {
+#ifdef LOCAL
+      cout << "-- Case #" << nc << " --\n";
+      solve();
+#else
+      solve();
+#endif
+   }
+   return 0;
+}
