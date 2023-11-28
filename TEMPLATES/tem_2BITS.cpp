512ae140de9bbb1a43e27334f81691c80296f81a
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-11-28 02:09:30 -0500

    Templates

--- TEMPLATES/tem_2BITS.cpp
+++ TEMPLATES/tem_2BITS.cpp
@@ -21,7 +21,7 @@ public:
       int tt = 1, nc = 0;
       cin >> tt;
       while (tt--) {
-      #ifdef DEBUG
+      #ifdef LOCAL
          cout << "Case #" << ++nc << ":\n";
          Solution();
       #else
@@ -37,47 +37,5 @@ int main() {
    RPCProblemSolver cp;
    cp.TestCase();
    // cp.Solution();
-   return 0;
-}
-_____
-/**
- *   █▀█  █▀▀▄ ─▀─ ▀▀█▀▀ █▀▀ ▄▀ ▀▄
- *   ─▄▀  █▀▀▄ ▀█▀ ──█── ▀▀█ █─ ─█
- *   █▄▄  ▀▀▀─ ▀▀▀ ──▀── ▀▀▀ ▀▄ ▄▀
-**/
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ll  long long
-#define br  '\n'
-
-class tem_2BITS {
-public:
-   static const int N = 1e1 + 20;
-   int a[N], n;
-
-   void Solution() {
-   }
-
-   void TestCase() {
-      int tt = 1, nc = 0;
-      cin >> tt;
-      while (tt--) {
-      #ifdef DEBUG
-         cout << "Case #" << ++nc << ":\n";
-         Solution();
-      #else
-         Solution();
-      #endif
-      }
-   }
-};
-
-int main() {
-   ios::sync_with_stdio(false);
-   cin.tie(0);
-   tem_2BITS cp;
-   // cp.TestCase();
-   cp.Solution();
    return 0;
 }
