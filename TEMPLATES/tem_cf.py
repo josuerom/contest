fc0be423a588cb60c41a5a80c4ca6bae69774140
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-14 15:28:13 -0500

    New templates

---
+++ TEMPLATES/tem_cf.py
@@ -0,0 +1,10 @@
+#  author:  $%U%$ - created: $%D%$/$%M%$/$%Y%$
+import sys as io
+
+def rli(): return list(map(int, io.stdin.readline().split()))
+def rls(): return list(map(str, io.stdin.readline().split(" ")))
+def rlc(): return io.stdin.readline().strip()
+def rint(): return int(io.stdin.readline())
+def pl(arr): print(' '.join(map(str, arr)))
+def pll(arr): print('\n'.join(map(str, arr)))
+
