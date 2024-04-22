db215e34069416cb3470af1529870c730826843d
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-21 21:17:30 -0500

    Update tem_cf.py

--- TEMPLATES/tem_cf.py
+++ TEMPLATES/tem_cf.py
@@ -1,10 +1,15 @@
-#  author:  $%U%$ - created: $%D%$/$%M%$/$%Y%$
-import sys as io
+#  author: $%U%$ - created: $%D%$/$%M%$/$%Y%$
+import sys
 
-def rli(): return list(map(int, io.stdin.readline().split()))
-def rls(): return list(map(str, io.stdin.readline().split(" ")))
-def rlc(): return io.stdin.readline().strip()
-def rint(): return int(io.stdin.readline())
-def pl(arr): print(' '.join(map(str, arr)))
-def pll(arr): print('\n'.join(map(str, arr)))
+def rli(): return list(map(int, sys.stdin.readline().split()))
+def rls(): return list(map(str, sys.stdin.readline().split()))
+def rlc(): return sys.stdin.readline().strip()
+def rint(): return int(sys.stdin.readline())
+def pl(ls): print(' '.join(map(str, ls)))
+def plb(ls): print('\n'.join(map(str, ls)))
 
+def solve():
+   n = rint()
+
+for _ in range(rint()):
+   print(solve())
