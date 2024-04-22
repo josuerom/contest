ad9fd5408bd5ab8439b06de84a72a2b9f9a8ebb3
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-21 21:21:28 -0500

    Update template.py

---
+++ TEMPLATES/template.py
@@ -0,0 +1,15 @@
+"""
+   ░░█ █▀█ █▀ █░█ █▀▀ █▀█ █▀█ █▀▄▀█
+   █▄█ █▄█ ▄█ █▄█ ██▄ █▀▄ █▄█ █░▀░█
+"""
+import sys
+
+def rli(): return list(map(int, sys.stdin.readline().split()))
+def rls(): return list(map(str, sys.stdin.readline().split()))
+def rlc(): return sys.stdin.readline().strip()
+def rint(): return int(sys.stdin.readline())
+def pl(ls): print(' '.join(map(str, ls)))
+def plb(ls): print('\n'.join(map(str, ls)))
+
+for _ in range(rint()):
+   n = rint()
