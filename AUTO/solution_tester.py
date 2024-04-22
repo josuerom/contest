1ca8cdf2cd3cff8f743789d018dabf665a8e64b8
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-21 20:50:15 -0500

    Auto

--- AUTO/solution_tester.py
+++ AUTO/solution_tester.py
@@ -1,11 +1,11 @@
 #  author: josuerom - created: 21/04/24 20:00:03
+import sys
 import os
 import subprocess
 from termcolor import colored
 
 
-def tipo_de_programa():
-   programa = input("Nombre del programa: ").strip()
+def main(programa):
    extension = programa.split(".")
    if programa.endswith(".py"):
       ejecutar_python(programa)
@@ -25,7 +25,8 @@ def ejecutar_python(programa):
          break
       with open(entrada_estandar, "r") as contenido_archivo_de_entrada:
          entrada_estandar_datos = contenido_archivo_de_entrada.read().strip()
-      proceso = subprocess.Popen(["python3", programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+      proceso = subprocess.Popen(["python3", programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE, text=True)
       salida_generada, _ = proceso.communicate(input=entrada_estandar_datos)
       with open(respuesta_correcta, "r") as contenido_archivo_de_entrada:
          salida_esperada = contenido_archivo_de_entrada.read().strip()
@@ -43,22 +44,24 @@ def compilar_y_ejecutar_cpp(programa):
          entrada_estandar = f"samples/in{i}.txt"
          respuesta_correcta = f"samples/ans{i}.txt"
          if not os.path.exists(entrada_estandar):
-            break
+               break
          with open(entrada_estandar, "r") as contenido_archivo_de_entrada:
-            entrada_estandar_datos = contenido_archivo_de_entrada.read().strip()
-         proceso = subprocess.Popen(["./" + programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+               entrada_estandar_datos = contenido_archivo_de_entrada.read().strip()
+         proceso = subprocess.Popen(["./" + programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, text=True)
          salida_generada, _ = proceso.communicate(input=entrada_estandar_datos)
          with open(respuesta_correcta, "r") as contenido_archivo_de_entrada:
-            salida_esperada = contenido_archivo_de_entrada.read().strip()
+               salida_esperada = contenido_archivo_de_entrada.read().strip()
          if salida_generada.strip() == salida_esperada.strip():
-            print(colored(f"Test case {i} passed ✅", "green"))
+               print(colored(f"Test case {i} passed ✅", "green"))
          else:
-            print(colored("Wrong answer case {i} ❌", "red"))
-            print(f"Output:\n{salida_generada}", end="\n")
-            print(f"Answer:\n{salida_esperada}")
+               print(colored("Wrong answer case {i} ❌", "red"))
+               print(f"Output:\n{salida_generada}", end="\n")
+               print(f"Answer:\n{salida_esperada}")
 
    nombre_ejecutable = programa.replace(".cpp", "")
-   proceso_compilacion = subprocess.Popen(["g++ -std=c++17 -O2 -DLOCAL", programa, "-o", nombre_ejecutable], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+   proceso_compilacion = subprocess.Popen(["g++ -std=c++17 -O2 -DLOCAL", programa, "-o", nombre_ejecutable],
+                                          stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
    _, errores_compilacion = proceso_compilacion.communicate()
    if proceso_compilacion.returncode == 0:
       ejecutar(nombre_ejecutable)
@@ -75,7 +78,8 @@ def ejecutar_java(programa):
          break
       with open(entrada_estandar, "r") as contenido_archivo_de_entrada:
          entrada_estandar_datos = contenido_archivo_de_entrada.read().strip()
-      proceso = subprocess.Popen(["java", programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+      proceso = subprocess.Popen(["java", programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE, text=True)
       salida_generada, _ = proceso.communicate(input=entrada_estandar_datos)
       with open(respuesta_correcta, "r") as contenido_archivo_de_entrada:
          salida_esperada = contenido_archivo_de_entrada.read().strip()
@@ -88,4 +92,7 @@ def ejecutar_java(programa):
 
 
 if __name__ == "__main__":
-   tipo_de_programa()
+   """Para vereficar, ejecute:
+      python3 solution_tester.py HolaMundo.cpp
+   """
+   main(sys.argv[1])
