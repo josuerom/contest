fcc1572a6bdaff7065875e8530d2c683f47e60dd
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-21 21:01:00 -0500

    Auto

--- AUTO/generate_contest_rpc_windows.py
+++ AUTO/generate_contest_rpc_windows.py
@@ -2,31 +2,27 @@
 # Autor: josuerom - Fecha: 27/06/23 15:34:40
 import os
 import glob
+import re
 import shutil
-import time
 import subprocess
 
-
-def obtenerPDF(directorio_pdf, round):
-    nombre_pdf = glob.glob(os.path.join(
-        directorio_pdf, f"*RPC{round}*.pdf"))
-    if len(nombre_pdf) > 0:
-        return nombre_pdf[0]
-    else:
-        return None
-
+def obtener_pdf(directorio_pdf, round):
+    archivos_pdf = glob.glob(os.path.join(directorio_pdf, "*.pdf"))
+    for archivo_pdf in archivos_pdf:
+        if re.search(r"(rpc|set|problem|{round})", archivo_pdf, re.IGNORECASE):
+            return archivo_pdf
+    return None
 
 def crear_dirs(round):
     ruta_base_rpc = r"d:\workspace\contest\rpc"
-
     ruta_base = os.path.join(ruta_base_rpc, "2024")
+
     if not os.path.exists(ruta_base):
         os.makedirs(ruta_base)
 
     nombre_dir = f"Rnd{round}"
     ruta_dir = os.path.join(ruta_base, nombre_dir)
     dir_pdf = r"c:\users\josuerom\downloads"
-    # dir_pdf = r"c:\users\jr3\descargas"
 
     if os.path.exists(ruta_dir):
         print(f"El round ya existe 😞.")
@@ -34,13 +30,13 @@ def crear_dirs(round):
     else:
         os.makedirs(ruta_dir)
 
-    nombre_pdf = obtenerPDF(dir_pdf, round)
+    nombre_pdf = obtener_pdf(dir_pdf, round)
 
-    if nombre_pdf is not None:
+    if nombre_pdf:
         nombre_del_pdf = os.path.basename(nombre_pdf)
         get_pdf = os.path.join(dir_pdf, nombre_del_pdf)
         shutil.move(get_pdf, ruta_dir)
-        print(f"Se han creado los archivos.")
+        print("Se han creado los archivos.")
     else:
         print("\nNo se encontró el archivo PDF en ~\Descargas o ~\Downloads.")
         option = int(input("Presione 1 para continuar o 2 para salir -> "))
@@ -59,12 +55,9 @@ def crear_dirs(round):
         with open(archivo_base, 'x'):
             pass
 
-    print("\nSe iniciará VSCode 😁😁...", end='\n')
-
-    comando = f"code-insiders {ruta_dir}"
-    # comando = f"code {ruta_dir}"
-    subprocess.run(comando, shell=True)
-
+    print("\nSe iniciará VSCode 😁😁...")
+    subprocess.run(f"code-insiders {ruta_dir}", shell=True)
+    subprocess.run("taskkill /f /im cmd.exe", shell=True)
 
 if __name__ == '__main__':
     s = input("Número del round -> ")
