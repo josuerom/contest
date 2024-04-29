3786c300a74cebf0d8dda59b7b36f9675f969ea9
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-29 11:37:52 -0500

    Update tem_cpeditor.cpp

--- TEMPLATES/tem_cpeditor.cpp
+++ TEMPLATES/tem_cpeditor.cpp
@@ -3,16 +3,9 @@
 #define br  '\n'
 
 using namespace std;
-const int N = 10 + int(1e1);
-
-void solve() {
-}
 
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(0);
-   int tt = 1;
-   // cin >> tt;
-   while (tt--) solve();
    return 0;
 }
