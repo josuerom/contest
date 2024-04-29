c65b5bc982439d6001b34645c02d1c038aa1ad66
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-29 11:36:44 -0500

    Update tem_cf.py

--- TEMPLATES/tem_cf.py
+++ TEMPLATES/tem_cf.py
@@ -1,11 +1,12 @@
 #  author: $%U%$ - created: $%D%$/$%M%$/$%Y%$
 import sys
 
-def rli(): return list(map(int, sys.stdin.readline().split(r"\s+")))
-def rls(): return list(map(str, sys.stdin.readline().split(r"\s+")))
-def rlc(): return list(sys.stdin.readline())
-def rint(): return int(sys.stdin.readline())
+def rstr(): return sys.stdin.readline()
+def rint(): return int(rstr())
+def rli():  return list(map(int, rstr().split()))
+def rls():  return list(map(str, rstr().split(r"\s+")))
+def rlc():  return list(rstr())
 def pl(ls): print(' '.join(map(str, ls)))
-def plb(ls): print('\n'.join(map(str, ls)))
+def pb(ls): print('\n'.join(map(str, ls)))
 
 for _ in range(rint()):
