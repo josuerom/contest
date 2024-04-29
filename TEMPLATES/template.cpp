d9525e62e3b86e1088a33937deee9ef788d38764
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-29 11:39:10 -0500

    Update template.cpp

--- TEMPLATES/template.cpp
+++ TEMPLATES/template.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 10 + 1e1;
+const int N = int(1e1) + 10;
 int a[N], n;
 
 void solve() {
@@ -14,15 +14,15 @@ void solve() {
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(0);
-   int tt = 1, nc = 1;
+   int tt = 1;
    cin >> tt;
-   while (tt--) {
 #ifdef LOCAL
+   int nc = 1;
+   while (tt--) {
       cout << "Case #" << nc++ << ":\n";
       solve();
-#else
-      solve();
-#endif
    }
-   return 0;
+#else
+   while (tt--) solve();
+#endif
 }
